56ad9303ec4fc7d105f14033c2edec907cdeb92c
Saveliy <p89617625637@gmail.com>
2026-04-02 22:49:36 +0500

    Доделана модель и админка юзера

--- p2p/users/admin.py
+++ p2p/users/admin.py
@@ -1,11 +1,37 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
 class UserAdmin(BaseUserAdmin):
     model = User
-    list_display = ['email', 'username',]
-    # todo: дописать админку
+    readonly_fields = ['last_login', 'created_at', 'updated_at',]
+    list_display = [
+        'email',
+        'username',
+        'phone_number',
+        'country',
+        'region',
+        'city',
+        'district',
+        'created_at',
+        'updated_at'
+    ]
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone_number', 'profile_picture')}),
+        (_('Address'), {'fields': ('country', 'region', 'city', 'district', 'street', 'building', 'apartment')}),
+        (_('Important dates'), {'fields': ('last_login', 'created_at', 'updated_at')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff'),
+        }),
+    )
+    list_filter = ['country', 'region', 'city', 'district', 'created_at', 'updated_at']
+    search_fields = ['email', 'username', 'phone_number']
+    ordering = ['email',]
 
 admin.site.register(User, UserAdmin)
--- p2p/users/models.py
+++ p2p/users/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator
 
 class User(AbstractUser):
     # username, first_name, last_name
     email = models.EmailField(_('email address'), unique=True)
-    phone_number = models.CharField(_('phone number'), max_length=20, blank=True)
-    profile_picture = models.ImageField(_('profile picture'), upload_to='pfp/', blank=True, null=True)
+    phone_number = models.CharField(
+        _('phone number'),
+        max_length=20,
+        blank=True,
+        validators=[
+            RegexValidator(
+                regex=r'^\+?1?\d{9,15}$',
+                message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+            )
+        ]
+    )
+    profile_picture = models.ImageField(_('profile picture'), upload_to='media/pfp/', blank=True, null=True)
     
+    # todo: добавить TextChoices для стран, регионов, городов, т.п.
     country = models.CharField(_('country'), max_length=256)
     region = models.CharField(_('region'), max_length=256)
     city = models.CharField(_('city'), max_length=256)
@@ -16,8 +28,8 @@ class User(AbstractUser):
     building = models.CharField(_('building'), max_length=256)
     apartment = models.CharField(_('apartment'), max_length=256)
 
-    created_at = models.DateField(_('creation date'), auto_now_add=True)
-    updated_at = models.DateField(_('last update date'), auto_now=True)
+    created_at = models.DateTimeField(_('creation date'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('last update date'), auto_now=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
