35ef73dd96765eaf9f74e819d5d539724ac79be1
Владислав Маяковский <mayachok666228@gmail.com>
2026-04-05 00:01:04 +0500

    Допил админки

--- p2p/p2p/settings.py
+++ p2p/p2p/settings.py
@@ -147,6 +147,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
