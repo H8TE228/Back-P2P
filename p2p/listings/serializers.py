ed292eb8787fd1635cd9b7bb9ec50985093fb507
unique1nickname <124551653+unique1nickname@users.noreply.github.com>
2026-04-12 21:20:25 +0500

    поле для календаря доступности

--- p2p/listings/serializers.py
+++ p2p/listings/serializers.py
@@ -1,6 +1,16 @@
 from rest_framework import serializers
 from .models import Category, Listing, ListingImage
 
+class AvaliabilityCalendarSlotSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+    start = serializers.DateTimeField()
+    end = serializers.DateTimeField()
+
+    def validate(self, data):
+        if data['start'] >= data['end']:
+            raise serializers.ValidationError("Конец не может быть перед началом")
+        return data
+
 class ListingImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ListingImage
@@ -35,7 +45,7 @@ class ListingSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'title', 'description', 'price', 'category', 'category_name',
             'owner', 'owner_name', 'status', 'created_at', 'updated_at',
-            'images', 'new_images'
+            'images', 'new_images', 'avaliability_calendar',
         ]
         read_only_fields = ['owner', 'created_at', 'updated_at', 'category_name', 'owner_name']
 
@@ -56,3 +66,18 @@ class ListingSerializer(serializers.ModelSerializer):
             ListingImage.objects.create(listing=instance, image=image_data)
         
         return instance
+    
+    def validate_avaliability_calendar(self, value):
+        serializer = AvaliabilityCalendarSlotSerializer(data=value, many=True)
+        if not serializer.is_valid():
+            raise serializers.ValidationError(serializer.errors)
+        
+        sorted_dates = sorted(value, key=lambda x: x['start'])
+        for i in range(len(sorted_dates) - 1):
+            current_slot = sorted_dates[i]
+            next_slot = sorted_dates[i+1]
+            if next_slot['start'] < current_slot['end']:
+                raise serializers.ValidationError(
+                    f'Интервалы пересекаются: end {current_slot['end']} накладывается на {next_slot['start']}'
+                )
+        return value
