22e6bc4adfdecfea32d3276409781c9fca9f4714
Saveliy <p89617625637@gmail.com>
2026-05-04 13:01:49 +0500

    feat/add fk name fields for transaction serializer

--- p2p/transactions/serializers.py
+++ p2p/transactions/serializers.py
@@ -2,13 +2,19 @@ from rest_framework import serializers
 from .models import Transaction
 
 class TransactionSerializer(serializers.ModelSerializer):
+    item_name = serializers.ReadOnlyField(source='item.name')
+    renter_name = serializers.ReadOnlyField(source='renter.username')
+    owner_name = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Transaction
         fields = [
             'id', 'owner', 'renter', 'item', 'rented_at',
             'status', 'returned_at',
+            'item_name', 'renter_name', 'owner_name',
         ]
         read_only_fields = [
             'id', 'owner', 'renter', 'item', 'rented_at',
             'status', 'returned_at',
+            'item_name', 'renter_name', 'owner_name',
         ]
